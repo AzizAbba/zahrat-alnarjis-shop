6c35757bc34480fc12057762283fc17269833463
AzizAbba <azizabboud00@gmail.com>
2025-03-30 17:34:02 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/zahrat-alnarjis-shop/', // Replace this with your GitHub repository name
+});
